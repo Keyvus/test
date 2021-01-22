f9b0eadd1e4eb1020080f02ad1ad969fe543e78f
keyvus <keyvusleon23@gmail.com>
2021-01-22 14:32:10 -0600

    GIT Practice Version 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("We are testing git");
+		
+		String a = "We are now tesing the ";
+		String b  =  "Test 2 varible";
+		
+		System.out.println(a+b);
+		
 
 	}
 
